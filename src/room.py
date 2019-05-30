dba336bd9bfdd06fc2a95e3f1cb4f872d48bd655
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-05-29 22:35:46 -0400

    Made room class args optional.

--- src/room.py
+++ src/room.py
@@ -5,21 +5,21 @@ class Room:
  def __init__(self, 
               name, 
               desc, 
-              cardinal_dir,
-              items, 
-              monsters,
-              current_player,
-              n_to,
-              s_to,
-              w_to,
-              e_to):
+              cardinal_dir = None,
+              items = None, 
+              monsters = None,
+              current_player = None,
+              n_to = None,
+              s_to = None,
+              w_to = None,
+              e_to = None):
   self.name = name
   self.desc = desc
-  self.cardinal_dir = '' if cardinal_dir is None else cardinal_dir
+  self.cardinal_dir = cardinal_dir if cardinal_dir is not None else cardinal_dir
   items = {} if items is None else items
   monsters = {} if items is None else items
-  self.current_player = current_player
-  self.n_to = n_to
-  self.s_to = s_to
-  self.w_to = w_to
-  self.e_to = e_to
+  self.current_player = current_player if current_player is not None else current_player
+  self.n_to = n_to if n_to is not None else n_to
+  self.s_to = s_to if s_to is not None else s_to
+  self.w_to = w_to if w_to is not None else w_to
+  self.e_to = e_to if w_to is not None else w_to
