78217f4917fbc5b7288ac3c3ac52fde14e6d250a
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-05-29 23:23:35 -0400

    Adding initial player character.

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
  
@@ -38,6 +39,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+rogue = Player('Constantine', 100, 5, 10, 100)
 # input_data = input('')
 def repl_parser():
  n = 'n'
--- src/player.py
+++ src/player.py
@@ -9,11 +9,11 @@ class Player:
               defense, 
               magic, 
               pouch,
-              current_room):
+              current_room = None):
   self.name = name
   self.health = health
   self.attack = attack
   self.defense = defense
   self.magic = magic
   pouch = {} if pouch is None else pouch
-  self.current_room = current_room
+  self.current_room = current_room if current_room is not None else current_room
