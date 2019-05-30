7bd011406ec31c86da0fa2c06fe760957edd103b
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-05-29 21:27:33 -0400

    Minimal player class created.

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+ def __init__(self, name, health, attack, defense, magic, pouch):
+  self.name = name
+  self.health = health
+  self.attack = attack
+  self.defense = defense
+  self.magic = magic
