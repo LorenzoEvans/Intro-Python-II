6335ed05457ade14dcff119af6fe1efc2763e5d1
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-05-30 23:37:50 -0400

    Adding item classes.

--- src/items.py
+++ src/items.py
@@ -7,9 +7,39 @@ class Item:
               defense = None):
   self.name = name
   self.desc = desc
-  self.type = type
-  self.attack = attack
-  self.defense = defense
+  self.type = type if type is not None else type
+  self.attack = attack if attack is not None else attack
+  self.defense = defense if defense is not None else defense
 
 class Potion(Item):
- def __init__(self)
+ def __init__(self,
+              name,
+              desc,
+              type):
+  self.name = name
+  self.desc = desc
+  self.type = type
+
+class Weapon(Item):
+ def __init__(self,
+              name,
+              desc,
+              type,
+              attack):
+  self.name = name
+  self.desc = desc
+  self.type = type
+  self.attack = attack
+ 
+class Armor(Item):
+ def __init__(self,
+              name,
+              desc,
+              type,
+              defense):
+  self.name = name
+  self.desc = desc
+  self.type = type
+  self.defense = defense
+
+# class Spell
