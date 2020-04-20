5f36745784d3e523d1aae9a73fd7dff0fb19b63b
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-04-19 21:09:35 -0700

    Setting up for unit testing

---
+++ game_test/__init__.py
@@ -0,0 +1,11 @@
+
+import unittest
+from game.player import Player 
+from game.room import Room
+from game.item_set.items import Item, MediumPotion, LargePotion, Weapon, BroadSword, LongSword, Armor
+from game.item_set.potions import Elixir
+from game.item_set.armor import Shield
+from game.item_set.spell import Fire_Spell, Ice_Spell, Water_Spell, Earth_Spell, Air_Spell
+
+if __name__ == '__main__':
+    unittest.main()
