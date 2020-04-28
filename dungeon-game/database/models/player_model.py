36206885734e9ad7437e6a7c30601b088243d12e
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-04-27 20:02:57 -0700

    Still shipppinngggg

--- dungeon-game/database/models/player_model.py
+++ dungeon-game/database/models/player_model.py
@@ -1,4 +1,19 @@
 from ..app import database
+# What is the relationship between players and items?
+# A player can have many items obviously, but can an item, be held by many players? ?_?
+# This depends on how we program the ownership of items.
+# We don't want items to be held by more than one character.
+# What difficulties does this pose?
+  # * Generating unique ids for identical items.
+    # * We may backtrack here because, how will we distinguish two
+    # identical items to generate unique ids for them without the unique id?
+    # We have to make id generation a part of the class instantiation,
+    # which requires something to exist outside of the classes, but
+    # capable of altering their state?
+    # Are we over thinking?
+    # We can distinguish between the object in the database, and say,
+    # an instantiation of that object, via the difference in logic that handles,
+    # retrieval from db, vs in game interaction.
 
 class Player(database.Model):
   __tablename__ = "players"
